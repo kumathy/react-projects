e55ab3d984c784a4a4331bba4d6cd818c788f28e
anh-tran2106 <tranlamtuananh2003@gmail.com>
2025-10-27 15:33:33 -0300

    create badge component (and use tailwindcss cuz y not

--- component_library/src/App.jsx
+++ component_library/src/App.jsx
@@ -1,6 +1,21 @@
-export default function App() {
-  return (
-    <p>App</p>
-  )
-}
+import Badge from "./components/Badge";
 
+export default function App() {
+  const colors = [
+    "gray",
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+  ];
+  const badges = colors.map((color) => (
+    <Badge color={color} shape="pill">
+      Badge
+    </Badge>
+  ));
+
+  return <main>{badges}</main>;
+}
